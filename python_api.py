4228c4a5d934d7f06b13c5c4e6bbd6d6075f56de
Byung kwon Kim <byungkwon96@gmail.com>
2022-08-08 13:49:36 +0900

    update python_api

--- python_api.py
+++ python_api.py
@@ -9,37 +9,22 @@ BASE_URL = "https://api.finance.naver.com/service/itemSummary.nhn?itemcode="
 l = time.ctime().split()
 today = l[-1] + "_" + l[1] + "_" + l[2]
 
+# lets try to find all the code number and save it into the list
+stock_code = []  # 코스닥, 코스피 코드
+with open("stock_codes.txt", "r") as f:
+    stock_code = [l.rstrip("\n") for l in f]
 
 # 날짜 파일에다 저장
-stock_code = ["001120", "005930"]  # 코스닥, 코스피 코드
 data = {}  # 나중에 저장할 딕션어리 json (format)
 with open(f"{today}.txt", "w") as f:
     for s in stock_code:
         url = BASE_URL + s
         response = requests.get(url)
-        res = response.json()
-        data[s] = res
+        if response.status_code == 200:
+            try:
+                res = response.json()
+                data[s] = res
+            except:
+                continue
 
     f.write(json.dumps(data, indent=6))
-
-
-"""
-# lets try to find all the code number and save it into the list
-stock_code = []
-with open("stock_codes.txt", "r") as f:
-    for line in f:
-        stock_code.append(str(line))
-
-data = {}
-
-for s in stock_code:
-    url = BASE_URL + s
-    response = requests.get(url)
-    if response.status_code == 200:
-        try:
-            res = response.json()
-            data[s] = res
-        except:
-            continue
-
-"""
