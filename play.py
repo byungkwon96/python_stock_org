cf8ad00699a77e0a22927e27aa29bc82fe895361
Byung kwon Kim <byungkwon96@gmail.com>
2022-08-10 12:37:43 +0900

    organizing etf info

---
+++ play.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+response = requests.get("https://finance.naver.com/api/sise/etfItemList.nhn")
+data = json.loads(response.text)
+print(data)
+dic = {}
+
+for i in data["result"]["etfItemList"]:
+    dic[i["itemcode"]] = {"itemname": i["itemname"], "total_num_stock": i["quant"]}
+
+with open("./data/etf/etf_name.json", "w") as f:
+    f.write(json.dumps(dic, ensure_ascii=False, indent=6))
